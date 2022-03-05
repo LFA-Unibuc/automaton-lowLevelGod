35a83e3e4d9c32b39a2268348123b3cda47ffc23
lowLevelGod <petrepopa19@gmail.com>
2022-03-05 20:21:11 +0200

    automatonv1

--- automaton.py
+++ automaton.py
@@ -1,16 +1,42 @@
-class Automaton():
+from dbus import ValidationException
 
+
+class Automaton():
+    
     def __init__(self, config_file):
         self.config_file = config_file
+        file = open(self.config_file)
+        d = {}
+        current_set = ""
+        for line in file.readlines():
+            line = line.rstrip().strip()
+            if '#' not in line:
+                if ':' in line:
+                    line = line.split(":", maxsplit=1)[0].rstrip()
+                    d[line] = []
+                    current_set = line
+                else:
+                    if line != "End":
+                        tple = []
+                        for x in line.split(","):
+                            tple.append(x.rstrip().strip())
+                        tple = tuple(tple)
+                        d[current_set].append(tple)
+        file.close()
+        self.data = d
         print("Hi, I'm an automaton!")
 
     def validate(self):
-        """Return a Boolean
-
-        Returns true if the config file is valid,
-        and raises a ValidationException if the config is invalid.
-        """
-        return "I can't tell if the config file is valid... yet!"
+        d = self.data
+        start_states = sum(1 for x in d["States"] if len(x) > 1 if x[1] == 'S')
+        if start_states > 1:
+            raise ValidationException("Only one starting state allowed !")
+        for t in d['Transitions']:
+            all_states = [x[0] for x in d['States']]
+            all_words = [x[0] for x in d['Sigma']]
+            if t[0] not in all_states or t[1] not in all_words or t[2] not in all_states:
+                raise ValidationException("Invalid transition")
+        return True
 
     def accepts_input(self, input_str):
         """Return a Boolean
@@ -28,7 +54,7 @@ class Automaton():
         """
         pass
     
-
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
+    a = Automaton('input.txt')
     print(a.validate())
+
